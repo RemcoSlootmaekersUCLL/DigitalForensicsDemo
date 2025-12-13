68452be85685f09a0cfc9fd37893d10423a197f7
RemcoSlootmaekersUCLL <r1035581@ucll.be>
2025-12-13 16:00:59 +0100

    Updates

--- _next/static/eyLkTmOS-4mbplBruLxDi/_buildManifest.js
+++ _next/static/eyLkTmOS-4mbplBruLxDi/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/DigitalForensicsDemo//_next/:path+",
-        "destination": "/DigitalForensicsDemo/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
